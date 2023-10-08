2e37dca7edb8ceadec480e63dd69972bb39b1bfa
ashley0416 <akok0009@student.monash.edu>
2023-10-08 20:09:11 +0800

    updated all

---
+++ vega_lite_vis.js
@@ -0,0 +1,15 @@
+var vg_1 = "visualization.vl.json"
+
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
+
+var vg_2 = "bubble_plot.vl.json"
+
+vegaEmbed("#bubble_plot", vg_2).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
